9058c40e7a52481ac7874bb3de2b6e43197e7762
rinne <180847792+rinnein@users.noreply.github.com>
2026-05-14 22:49:36 +0800

    feat: add tests for downgradeAdvancedPluginConfigurator

---
+++ tests/plugins.test.ts
@@ -0,0 +1,63 @@
+import { DanuniJsonAdapter, DanuniJsonTransformerConfigurator } from "@/adapters/index.ts";
+import { defaultUniDM, InitedUniDB, UniDB, type UniChunk, type UniDMObj } from "@/core/index.ts";
+import { downgradeAdvancedPluginConfigurator } from "@/plugins/downgradeAdvcanced.ts";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+
+const now = new Date();
+let udb: InitedUniDB;
+let chunk: UniChunk;
+let danuniJson: UniDMObj[];
+beforeAll(async () => {
+  udb = await new UniDB().init();
+  const ori = await udb.import(
+    DanuniJsonAdapter([
+      {
+        ...defaultUniDM,
+        ctime: now,
+        DMID: "",
+        content: "test",
+        extra: {
+          danuni: {
+            merge: {
+              count: 100,
+              duration: 10,
+              senders: [],
+              taolu_count: 100,
+              taolu_senders: [],
+            },
+          },
+        },
+      },
+      {
+        ...defaultUniDM,
+        ctime: now,
+        DMID: "",
+        platform: "bili",
+        extra: {
+          bili: {
+            mode: 7,
+            adv: '["0.355","0.27","0.8-0","0.6"," 真棒☺",0,0,"0.355",0,"500",0,0,"SimHei",1]',
+          },
+        },
+      },
+    ]),
+  );
+  chunk = await ori.plugin(downgradeAdvancedPluginConfigurator());
+  danuniJson = await chunk.export(DanuniJsonTransformerConfigurator());
+});
+afterAll(async () => {
+  await udb.close();
+});
+
+describe("弹幕降级", () => {
+  it("danuni.merge", () => {
+    const d = danuniJson[0];
+    console.info(d);
+    expect(d.content).equal("test x100");
+  });
+  it("bili.adv", () => {
+    const d = danuniJson[1];
+    console.info(d);
+    expect(d.content).equal("[B站高级弹幕] 真棒☺");
+  });
+});
