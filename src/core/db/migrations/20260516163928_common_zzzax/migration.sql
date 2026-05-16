275cafb150cddf1503418911a596ef48b201d7c7
rinne <180847792+rinnein@users.noreply.github.com>
2026-05-17 00:39:58 +0800

    chore: reset migration files

--- src/core/db/migrations/20260516163928_common_zzzax/migration.sql
+++ src/core/db/migrations/20260516163928_common_zzzax/migration.sql
@@ -1,6 +1,12 @@
 CREATE TYPE "dm_attr" AS ENUM('Protect', 'FromLive', 'HighLike', 'Compatible', 'Reported', 'Unchecked', 'HasEvent', 'Hide');--> statement-breakpoint
 CREATE TYPE "mode" AS ENUM('Normal', 'Bottom', 'Top', 'Reverse', 'Ext');--> statement-breakpoint
 CREATE TYPE "pool" AS ENUM('Def', 'Sub', 'Adv', 'Ix');--> statement-breakpoint
+CREATE TABLE "chunk_danmakus" (
+	"id" bigserial PRIMARY KEY,
+	"chunkID" integer NOT NULL,
+	"DMID" text NOT NULL
+);
+--> statement-breakpoint
 CREATE TABLE "chunks" (
 	"id" serial PRIMARY KEY,
 	"fromConverted" boolean DEFAULT false NOT NULL
@@ -8,8 +14,7 @@ CREATE TABLE "chunks" (
 --> statement-breakpoint
 CREATE TABLE "danmakus" (
 	"SOID" text NOT NULL,
-	"chunkID" serial,
-	"DMID" text,
+	"DMID" text PRIMARY KEY,
 	"progress" integer NOT NULL,
 	"mode" "mode" NOT NULL,
 	"fontsize" smallint NOT NULL,
@@ -21,8 +26,8 @@ CREATE TABLE "danmakus" (
 	"pool" "pool" NOT NULL,
 	"attr" "dm_attr"[] NOT NULL,
 	"platform" text,
-	"extra" jsonb,
-	CONSTRAINT "danmakus_pkey" PRIMARY KEY("DMID","chunkID")
+	"extra" jsonb
 );
 --> statement-breakpoint
-ALTER TABLE "danmakus" ADD CONSTRAINT "danmakus_chunkID_chunks_id_fkey" FOREIGN KEY ("chunkID") REFERENCES "chunks"("id");
+ALTER TABLE "chunk_danmakus" ADD CONSTRAINT "chunk_danmakus_chunkID_chunks_id_fkey" FOREIGN KEY ("chunkID") REFERENCES "chunks"("id");--> statement-breakpoint
+ALTER TABLE "chunk_danmakus" ADD CONSTRAINT "chunk_danmakus_DMID_danmakus_DMID_fkey" FOREIGN KEY ("DMID") REFERENCES "danmakus"("DMID");
