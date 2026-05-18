a5869989b8fe2f766c774076317db5f6cf72e8e2
rinne <180847792+rinnein@users.noreply.github.com>
2026-05-18 16:04:11 +0800

    test: add merge plugin test

--- tests/plugins.test.ts
+++ tests/plugins.test.ts
@@ -1,11 +1,41 @@
-import { DanuniJsonAdapter, DanuniJsonTransformerConfigurator } from "@/adapters/index.ts";
+import {
+  BiliXmlAdapter,
+  DanuniJsonAdapter,
+  DanuniJsonTransformerConfigurator,
+} from "@/adapters/index.ts";
 import { defaultUniDM, InitedUniDB, UniDB, type UniChunk, type UniDMObj } from "@/core/index.ts";
 import {
   getStatsTransformerConfigurator,
   downgradeAdvancedPluginConfigurator,
   getStatsUtil4getMost,
+  mergePluginConfigurator,
 } from "@/plugins/index.ts";
 import { afterAll, beforeAll, describe, expect, it } from "vitest";
+
+const xml = `<i>
+<chatserver>chat.bilibili.com</chatserver>
+<chatid>1156756312</chatid>
+<mission>0</mission>
+<maxlimit>2947</maxlimit>
+<state>0</state>
+<real_name>0</real_name>
+<source>k-v</source>
+<d p="13.213,1,25,16777215,1686314041,3,ff41173d,1335658005672492032">喜欢</d>
+<d p="13.213,1,25,16777215,1686590010,0,296b35b5,1337972999512832512">来了 哈哈~~</d>
+<d p="13.246,1,25,16777215,1686276875,0,5664cfc4,1335346233459549696">就是</d>
+<d p="13.266,1,25,16777215,1686283375,0,c7e6646f,1335400761013670912">什么鬼？</d>
+<d p="13.284,1,25,16777215,1686291338,0,38662881,1335467554877267456">哇哦</d>
+<d p="13.306,1,25,16777215,1686268410,0,4c01de10,1335275224983600896">试试</d>
+<d p="13.331,1,25,16777215,1686948453,3,56a3c5d5,1340979831550069760">不喜欢</d>
+<d p="13.374,1,25,16777215,1686300770,3,647fe355,1335546672880933888">不喜欢</d>
+<d p="13.376,1,25,16777215,1686297921,0,469d94b8,1335522778300134400">哦豁</d>
+<d p="13.419,1,25,8700107,1686268005,0,be402447,1335271828100244224">太酷啦</d>
+<d p="13.419,1,25,16777215,1686316828,3,7ffb6619,1335681385016736768">喜欢</d>
+<d p="13.459,1,25,16777215,1686299729,0,45834405,1335537942797634048">一般，不好看</d>
+<d p="13.462,1,25,16777215,1686302133,0,3cab672c,1335558106620590080">哈哈哈</d>
+<d p="13.481,1,25,16777215,1686297342,0,ce67fafd,1335517923728804864">？</d>
+<d p="13.499,1,25,16777215,1686301548,3,2848bf1c,1335553202649003264">不喜欢</d>
+</i>`;
 
 const now = new Date();
 let udb: InitedUniDB;
@@ -50,6 +80,7 @@ beforeAll(async () => {
   danuniJson = await chunk.export(DanuniJsonTransformerConfigurator());
 });
 afterAll(async () => {
+  await udb.shrink();
   await udb.close();
 });
 
@@ -74,4 +105,13 @@ describe("弹幕降级", () => {
     console.info(d);
     expect(d.content).toBe("[B站高级弹幕] 真棒☺");
   });
+});
+
+it("merge", async () => {
+  const c = await udb.import(BiliXmlAdapter(xml));
+  const merged = await c.plugin(mergePluginConfigurator(10));
+  const dms = await merged.export(DanuniJsonTransformerConfigurator({ minify: true }));
+  console.info(dms);
+  expect(dms.filter((d) => d.content === "喜欢").length).toBe(1);
+  expect(dms.filter((d) => d.content === "不喜欢").length).toBe(1);
 });
